db791a5caa4554f3994363fcd5da2515990433fb
Rudson_A <abraaorudson1@gmail.com>
2023-03-23 08:54:34 -0300

    prótotipo ypioca finalizado

--- app/main.js
+++ app/main.js
@@ -1,5 +1,10 @@
 const btnCalcular = document.getElementById('btn-calcular');
-const doses = [{dose: 1, cm: 1.5, limite: 2.5}, {dose: 2, cm: 2.5, limite: 3.5}]
+const doses = [{dose: 1, cm: 1.5, limite: 2.5}, {dose: 2, cm: 2.5, limite: 3.5}, {dose: 3, cm: 3.5, limite: 4.5},
+     {dose: 4, cm: 4.5, limite: 5.8}, {dose: 5, cm: 5.8, limite: 6.8}, {dose: 6, cm: 6.8, limite: 7.8},
+     {dose: 7, cm: 7.8, limite: 8.8}, {dose: 8, cm: 8.8, limite: 9.8}, {dose: 9, cm: 9.8, limite: 11}, 
+     {dose: 10, cm: 11, limite: 12}, {dose: 11, cm: 12, limite: 13}, {dose: 12, cm: 13, limite: 14},
+     {dose: 13, cm: 14, limite: 15}, {dose: 14, cm: 15, limite: 16}, {dose: 15, cm: 16, limite: 17},
+     {dose: 16, cm: 17, limite: 18}, {dose: 17, cm: 18, limite: 19.5}, {dose: 18, cm: 19.5, limite: 23}, {dose: 19, cm: 23, limite: 0}   ]
 
 
 
@@ -14,7 +19,7 @@ btnCalcular.addEventListener('click', ()=>{
 function resultadoNaTela(dose){
     const elementoResultado = document.getElementById('calculoResposta');
     elementoResultado.innerHTML =`
-    <h2>Contém ${dose} dose(s)</h2>
+    <h2 class="resposta">Contém ${dose} dose(s)</h2>
     `
 }
 
@@ -26,8 +31,10 @@ function calculoMedida(cm){
             doseDaGarrafa = dose.dose;
             console.log(doseDaGarrafa);
         } if(cm>dose.cm && cm < dose.limite){
-            doseDaGarrafa = (cm - dose.cm) + dose.dose;
+            doseDaGarrafa = (cm - dose.cm) / (dose.limite - dose.cm) + dose.dose;
         }
     });
     return doseDaGarrafa
 }
+
+

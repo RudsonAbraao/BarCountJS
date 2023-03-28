9fa750a2e03a2b993f99c0c8e965cd530aacf7df
Rudson_A <abraaorudson1@gmail.com>
2023-03-28 13:12:22 -0300

    botões resolvidos

--- app/main.js
+++ app/main.js
@@ -1,16 +1,32 @@
 const elementoResultado = document.getElementById('calculoResposta');
 const btnCalcular = document.getElementById('btn-calcular');
-const ypioca = [{dose: 1, cm: 1.5, limite: 2.5}, {dose: 2, cm: 2.5, limite: 3.5}, {dose: 3, cm: 3.5, limite: 4.5},
-     {dose: 4, cm: 4.5, limite: 5.8}, {dose: 5, cm: 5.8, limite: 6.8}, {dose: 6, cm: 6.8, limite: 7.8},
-     {dose: 7, cm: 7.8, limite: 8.8}, {dose: 8, cm: 8.8, limite: 9.8}, {dose: 9, cm: 9.8, limite: 11}, 
-     {dose: 10, cm: 11, limite: 12}, {dose: 11, cm: 12, limite: 13}, {dose: 12, cm: 13, limite: 14},
-     {dose: 13, cm: 14, limite: 15}, {dose: 14, cm: 15, limite: 16}, {dose: 15, cm: 16, limite: 17},
-     {dose: 16, cm: 17, limite: 18}, {dose: 17, cm: 18, limite: 19.5}, {dose: 18, cm: 19.5, limite: 23}, {dose: 19, cm: 23, limite: 0}   ]
+const ypioca = {nome: 'ypioca',doses:[{dose: 1, cm: 1.5, limite: 2.5},
+    {dose: 2, cm: 2.5, limite: 3.5},
+    {dose: 3, cm: 3.5, limite: 4.5},
+    {dose: 4, cm: 4.5, limite: 5.8},
+    {dose: 5, cm: 5.8, limite: 6.8},
+    {dose: 6, cm: 6.8, limite: 7.8},
+    {dose: 7, cm: 7.8, limite: 8.8},
+    {dose: 8, cm: 8.8, limite: 9.8},
+    {dose: 9, cm: 9.8, limite: 11}, 
+    {dose: 10, cm: 11, limite: 12},
+    {dose: 11, cm: 12, limite: 13},
+    {dose: 12, cm: 13, limite: 14},
+    {dose: 13, cm: 14, limite: 15},
+    {dose: 14, cm: 15, limite: 16},
+    {dose: 15, cm: 16, limite: 17},
+    {dose: 16, cm: 17, limite: 18},
+    {dose: 17, cm: 18, limite: 19.5},
+    {dose: 18, cm: 19.5, limite: 23},
+    {dose: 19, cm: 23, limite: 0}]}
+const smirnoff = {nome: 'smirnoff', doses:[{dose: 1, cm: 2.5, limite: 3.5}]}
 
+let bebidaSelecionada = '';
+const bebidas = [ypioca, smirnoff]
 
 btnCalcular.addEventListener('click', ()=>{
     const medida = document.getElementById('medida').value;
-    let dose = calculoMedida(medida);
+    let dose = calculoMedida(medida,bebidaSelecionada);
     resultadoNaTela(dose);
 });
 
@@ -21,9 +37,9 @@ function resultadoNaTela(dose){
     `
 }
 
-function calculoMedida(cm){
+function calculoMedida(cm, bebida){
     let doseDaGarrafa = 0;
-    ypioca.forEach(dose => {
+    bebida.doses.forEach(dose => {
         if(cm == dose.cm){
             console.log('deu certo');
             doseDaGarrafa = dose.dose;
@@ -32,7 +48,7 @@ function calculoMedida(cm){
             doseDaGarrafa = (cm - dose.cm) / (dose.limite - dose.cm) + dose.dose;
         }
     });
-    return doseDaGarrafa.toFixed(2)
+    return Number(doseDaGarrafa.toFixed(2))
 }
 
 
--- app/menu.js
+++ app/menu.js
@@ -1,38 +1,24 @@
 const btnMenu = document.getElementById('btnMenu');
-const btnYpioca = document.getElementById('btnYpioca');
-const btnSmirnoff = document.getElementById('btnSminnorf');
-const btnAbsolut = document.getElementById('btnAbsolut');
-const btnGordons = document.getElementById('btnGordons');
-const btnJoseCuervo = document.getElementById('btnJoseCuervo');
-const btnBacardi = document.getElementById('btnBacardi');
+const bebida = document.getElementById('grfSelecionada');
+
+let nodeListBtn = document.querySelectorAll('.btn');
+const botoes = Array.from(nodeListBtn);
+
+botoes.forEach(botao => {
+    botao.addEventListener('click', ()=>{
+        bebida.innerHTML= botao.innerHTML;
+
+        bebidas.forEach(bebida => {
+            if(bebida.nome === botao.innerHTML){
+                bebidaSelecionada = bebida;
+            }
+        });
+        return bebidaSelecionada;
+    })
+});
 
 btnMenu.addEventListener('click', ()=>{
     menuLateral.classList.toggle('oculto');
 });
 
-btnYpioca.addEventListener('click', ()=>{
-    titulo.innerHTML= 'Ypíoca 965ml';
-    elementoResultado.innerHTML= '';
-});
-btnSmirnoff.addEventListener('click', ()=>{
-    titulo.innerHTML= 'Smirnoff';
-    elementoResultado.innerHTML= '';
-});
 
-
-btnAbsolut.addEventListener('click', ()=>{
-    titulo.innerHTML= 'Absolut 1L';
-    elementoResultado.innerHTML= '';
-});
-btnGordons.addEventListener('click', ()=>{
-    titulo.innerHTML= 'Gordons';
-    elementoResultado.innerHTML= '';
-});
-btnJoseCuervo.addEventListener('click', ()=>{
-    titulo.innerHTML= 'Jose Cuervo';
-    elementoResultado.innerHTML= '';
-});
-btnBacardi.addEventListener('click', ()=>{
-    titulo.innerHTML= 'Bacardi';
-    elementoResultado.innerHTML= '';
-});

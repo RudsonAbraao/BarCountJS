6a05b64d61a029d2a886fe6df10d00acd265c32e
jaqueline <jaquelinenepomuceno.2019@gmail.com>
2023-03-30 10:01:53 -0300

    código funcionando usando api

--- app/main.js
+++ app/main.js
@@ -1,32 +1,33 @@
 const elementoResultado = document.getElementById('calculoResposta');
 const btnCalcular = document.getElementById('btn-calcular');
-const ypioca = {nome: 'ypioca',doses:[{dose: 1, cm: 1.5, limite: 2.5},
-    {dose: 2, cm: 2.5, limite: 3.5},
-    {dose: 3, cm: 3.5, limite: 4.5},
-    {dose: 4, cm: 4.5, limite: 5.8},
-    {dose: 5, cm: 5.8, limite: 6.8},
-    {dose: 6, cm: 6.8, limite: 7.8},
-    {dose: 7, cm: 7.8, limite: 8.8},
-    {dose: 8, cm: 8.8, limite: 9.8},
-    {dose: 9, cm: 9.8, limite: 11}, 
-    {dose: 10, cm: 11, limite: 12},
-    {dose: 11, cm: 12, limite: 13},
-    {dose: 12, cm: 13, limite: 14},
-    {dose: 13, cm: 14, limite: 15},
-    {dose: 14, cm: 15, limite: 16},
-    {dose: 15, cm: 16, limite: 17},
-    {dose: 16, cm: 17, limite: 18},
-    {dose: 17, cm: 18, limite: 19.5},
-    {dose: 18, cm: 19.5, limite: 23},
-    {dose: 19, cm: 23, limite: 0}]}
-const smirnoff = {nome: 'smirnoff', doses:[{dose: 1, cm: 2.5, limite: 3.5}]}
+// const ypioca = {nome: 'ypioca',doses:[{dose: 1, cm: 1.5, limite: 2.5},
+//     {dose: 2, cm: 2.5, limite: 3.5},
+//     {dose: 3, cm: 3.5, limite: 4.5},
+//     {dose: 4, cm: 4.5, limite: 5.8},
+//     {dose: 5, cm: 5.8, limite: 6.8},
+//     {dose: 6, cm: 6.8, limite: 7.8},
+//     {dose: 7, cm: 7.8, limite: 8.8},
+//     {dose: 8, cm: 8.8, limite: 9.8},
+//     {dose: 9, cm: 9.8, limite: 11}, 
+//     {dose: 10, cm: 11, limite: 12},
+//     {dose: 11, cm: 12, limite: 13},
+//     {dose: 12, cm: 13, limite: 14},
+//     {dose: 13, cm: 14, limite: 15},
+//     {dose: 14, cm: 15, limite: 16},
+//     {dose: 15, cm: 16, limite: 17},
+//     {dose: 16, cm: 17, limite: 18},
+//     {dose: 17, cm: 18, limite: 19.5},
+//     {dose: 18, cm: 19.5, limite: 23},
+//     {dose: 19, cm: 23, limite: 0}]}
+// const smirnoff = {nome: 'smirnoff', doses:[{dose: 1, cm: 2.5, limite: 3.5}]}
 
-let bebidaSelecionada = '';
-const bebidas = [ypioca, smirnoff]
+let bebidaSelecionada;
+// const bebidas = [ypioca, smirnoff]
 
 btnCalcular.addEventListener('click', ()=>{
     const medida = document.getElementById('medida').value;
     let dose = calculoMedida(medida,bebidaSelecionada);
+    console.log(bebidaSelecionada);
     resultadoNaTela(dose);
 });
 
@@ -40,12 +41,13 @@ function resultadoNaTela(dose){
 function calculoMedida(cm, bebida){
     let doseDaGarrafa = 0;
     bebida.doses.forEach(dose => {
-        if(cm == dose.cm){
+        console.log(bebida.doses);
+        if(cm == dose.inicio){
             console.log('deu certo');
             doseDaGarrafa = dose.dose;
             console.log(doseDaGarrafa);
-        } if(cm>dose.cm && cm < dose.limite){
-            doseDaGarrafa = (cm - dose.cm) / (dose.limite - dose.cm) + dose.dose;
+        } if(cm>dose.inicio && cm < dose.fim){
+            doseDaGarrafa = (cm - dose.inicio) / (dose.fim - dose.inicio) + dose.dose;
         }
     });
     return Number(doseDaGarrafa.toFixed(2))
--- app/teesteFetch.js
+++ app/teesteFetch.js
@@ -1,16 +1,18 @@
+const apiUrl = 'https://gist.githubusercontent.com/RudsonAbraao/b6fc3b9d69ec4c6f45828761f8ca95ec/raw/77d5e524c618520248085036ad7a46a1cb74fd23/garrafas.json';
 
 async function buscaGarrafas(botao){
     try{
-        var response = await fetch('https://gist.githubusercontent.com/RudsonAbraao/b6fc3b9d69ec4c6f45828761f8ca95ec/raw/77d5e524c618520248085036ad7a46a1cb74fd23/garrafas.json')
+        var response = await fetch(apiUrl)
         var bebidas = await response.json();
         let btn = botao.innerHTML;
-        await bebidas.forEach(bebida => {
+        console.log(btn);
+        bebidas.forEach(bebida => {
             let grfName = bebida.nome;
             if(grfName === btn){
                 bebidaSelecionada = bebida;
+                console.log(bebidaSelecionada)
             }
         });
-        return bebidaSelecionada;
 
     } catch(erro){
         console.log(erro);
@@ -25,6 +27,5 @@ botoes.forEach(botao => {
     botao.addEventListener('click', ()=>{
         bebida.innerHTML= botao.innerHTML;
         buscaGarrafas(botao)
-        console.log(bebidaSelecionada);
     })
 });

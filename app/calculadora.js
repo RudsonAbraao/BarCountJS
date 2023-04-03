1258f8957539068c26a89729387f51f2e1b301e1
Rudson_A <abraaorudson1@gmail.com>
2023-04-03 13:29:21 -0300

    aba de calculo finalizada

--- app/calculadora.js
+++ app/calculadora.js
@@ -1,10 +1,9 @@
+const btnAdd = document.getElementById('btnAdd');
 const btnCalc = document.getElementById('btnCalc');
 const abaCalc = document.getElementById('abaCalc');
-const btnAdd = document.getElementById('btnAdd');
 const calcLista = document.getElementById('calcLista');
 const textoTotal = document.getElementById('textTotal');
-let botoesDeleta;
-let qntGarrafa= 0;
+let qntGarrafa = 0;
 let totalDoses = 0
 
 
@@ -16,63 +15,54 @@ btnCalc.addEventListener('click', ()=>{
 
 function addGarrafaCalc(){
     qntGarrafa += 1;
+    criaGarrafa();
     contaDoseTotal();
-    botaoDelete();
-    console.log(totalDoses);
-    console.log(qntGarrafa);
     mostraDoseTotal();
+    botaoDelete();
+};
+
+function criaGarrafa(){
     calcLista.innerHTML += `
         <li class="calculadora__lista--item">
             <h3 class="calculadora__lista--item-texto"><span class="qntGarrafa">${qntGarrafa}ª</span> Garrafa: <span id="numDose">${dose}</span> Ds</h3>
-            <button id="btnDeleta" value="${dose}">X</button>
+            <button id="btnDeleta" class="btn__deleta" value="${dose}">X</button>
         </li>   
-    `
-    botaoDelete();
-    pegaDose(dose);
-    console.log(calcDoses);
-}
+    `;
+};
 
 function contaDoseTotal(){
     totalDoses += dose;
-}
+};
+
+function mostraDoseTotal(){
+    textoTotal.innerHTML = `
+        Total: ${totalDoses} Ds
+    `
+};
+
+function botaoDelete(){
+    let nodeListe = document.querySelectorAll('#btnDeleta');
+    let botoesDeleta = Array.from(nodeListe);
+    botoesDeleta.forEach(botao => {
+        botao.addEventListener('click',()=>{
+            let elementoDelete = event.target.parentNode;
+            let doseDelete = event.target.value;
+            totalDoses -= doseDelete;
+            elementoDelete.remove();
+            mostraDoseTotal();
+        } )
+    });
+
+};
 
 function zerarCalc(){
     totalDoses = 0;
     qntGarrafa = 0;
     calcLista.innerHTML = "";
     textoTotal.innerHTML = "Total: ... Ds";
-}
-
-function botaoDelete(){
-    let nodeListe = document.querySelectorAll('#btnDeleta');
-    botoesDeleta = Array.from(nodeListe);
-    botoesDeleta.forEach(botao => {
-        botao.addEventListener('click',()=>{
-            console.log(event.target.value);
-            
-            let elementoDelete = event.target.parentNode;
-            let nodeList = document.querySelectorAll('#numDose');
-            let doseDelete = event.target.value;
-            totalDoses -= doseDelete;
-            elementoDelete.remove();
-            console.log(totalDoses);
-            mostraDoseTotal();
-        } )
-    });
-    console.log(botoesDeleta);
+};
 
 
 
-}
-let calcDoses = [];
 
-function pegaDose(dose){
-    calcDoses.push(dose);
-}
-
-function mostraDoseTotal(){
-    textoTotal.innerHTML = `
-        Total: ${totalDoses} Ds
-    `
-}
 
